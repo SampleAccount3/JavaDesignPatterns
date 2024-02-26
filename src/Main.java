e077f6dfc63fef6c452b697b47f3f828398b0d25
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-02-26 14:05:20 +0800

    done

--- src/Main.java
+++ src/Main.java
@@ -11,18 +11,24 @@ public class Main {
         Greet(new IPrintable() {
             @Override
             public void Print(String message) {
-                System.out.println("This is a Anonymous Inner Class");
+                System.out.println("This is a Anonymous Inner Class" + message);
             }
         });
 
-        // lambda Expression
+        // lambda Expression syntax (arguments) -> Statements
         Greet( m -> System.out.println(m)); // Hello
 
         // the output is same as above
+        // this is a method reference
         Greet(System.out::println); // Hello
+
+        // assigning a lambda expression in a variable
+        IPrintable iPrintable = System.out::println;
+        iPrintable.Print("Iam the assigned Lambda");
     }
 
     public static void Greet(IPrintable iPrintable){
         iPrintable.Print("Hello");
+        iPrintable.Print("Hello2");
     }
 }
